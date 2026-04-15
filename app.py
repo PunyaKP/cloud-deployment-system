23d758b7a80bf832db67ecf1a7820d26b43a2dac
PunyaKP <punyakanehithlu@gmail.com>
2026-04-15 23:51:20 +0530

    Auto deploy commit

--- app.py
+++ app.py
@@ -26,32 +26,41 @@ def deploy():
     file.save(filepath)
 
     try:
-        # Git commands
-        os.system("git add .")
-        os.system('git commit -m "Auto deploy commit"')
-        os.system("git push origin main")
+            # Git commands
+            os.system("git add .")
+            os.system('git commit -m "Auto deploy commit"')
+            os.system("git push origin main")
 
-        # Simulate pipeline
-        print("Code pushed to GitHub repository")
-        time.sleep(1)
+             # Simulate pipeline
+            print("Code pushed to GitHub repository")
+            time.sleep(1)
 
-        print("Running Jenkins CI/CD pipeline...")
-        time.sleep(1)
+            print("Running Jenkins CI/CD pipeline...")
+            time.sleep(1)
 
-        print("Building Docker image...")
-        time.sleep(1)
+            print("Building Docker image...")
+            time.sleep(1)
 
-        print("Deploying to AWS EC2...")
-        time.sleep(1)
+            print("Deploying to AWS EC2...")
+            time.sleep(1)
 
-        return render_template(
-    "index.html",
-    message="🚀 Deployment Successful!",
-    logs="File uploaded...\nDocker build...\nDeployed to AWS..."
-)
+            logs = [
+                "📁 File uploaded",
+                "📦 Code pushed to GitHub",
+                "⚙️ Jenkins pipeline started",
+                "🐳 Docker image built",
+                "☁️ Deployed to AWS",
+                "✅ Live successfully"
+            ]
+            return render_template(
+                "index.html",
+                message="🚀 Deployment Successful!",
+                logs=logs,
+                progress=100
+            )
 
     except Exception as e:
-        return f"Error occurred: {e}"
+            return f"Error occurred: {e}"
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
